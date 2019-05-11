934011375d413e7931dddd1df11279c07bd4b00b
Doug Loomer <loomerds@gmail.com>
2019-05-11 19:23:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ function whisper(string) {
 function sayHiToGrandma(string) {
   if(string==="I love you, Grandma.") {
     return "I love you, too.";
-  } else if(){
-    if(string===string.toLowerCase()) {
-      return "I can't hear you!";
+  } else if(string===string.toLowerCase()){
+    return "I can't hear you!";
   } else {
     return "YES INDEED!";
   }
