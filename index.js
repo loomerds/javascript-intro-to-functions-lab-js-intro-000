edccdda58b797b9bd0e49330ae9b7e41bc03e7fc
Doug Loomer <loomerds@gmail.com>
2019-05-11 19:18:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function sayHiToGrandma(string) {
   if(string==="I love you, Grandma.")
     return "I love you, too."
 }
+
+function sayHiToGrandma(string) {
+  if(string===string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+}
